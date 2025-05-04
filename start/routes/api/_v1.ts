23b2d046f1ad243bb42e4f78682b6b3908162be3
Tom Gobich <tom@tomgobich.com>
2025-05-04 10:39:29 -0400

    lesson(3.3): api versioning

--- start/routes/api/_v1.ts
+++ start/routes/api/_v1.ts
@@ -1,10 +1,10 @@
-import OrganizationsController from "#controllers/api/organizations_controller";
+import OrganizationsController from "#controllers/api/v1/organizations_controller";
 import { middleware } from "#start/kernel";
 import router from "@adonisjs/core/services/router";
 
 router.group(() => {
   router.get('/organization', [OrganizationsController])
-}).prefix('/api').use([
+}).prefix('/api/v1').use([
   middleware.forceJsonResponse(),
   middleware.auth({ guards: ['api'] }),
   middleware.organization()

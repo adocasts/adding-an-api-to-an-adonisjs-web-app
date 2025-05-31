f7372fa52b7cd9ad2182136078ad5d2702183e0b
Tom Gobich <tom@tomgobich.com>
2025-05-31 08:25:47 -0400

    lesson(4.0): listing organization difficulties

---
+++ app/controllers/api/v1/difficulties_controller.ts
@@ -0,0 +1,30 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class DifficultiesController {
+  /**
+   * Display a list of resource
+   */
+  async index({ organization }: HttpContext) {
+    return organization.related('difficulties').query().orderBy('order')
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {}
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {}
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {}
+}
--- start/routes/api/_v1.ts
+++ start/routes/api/_v1.ts
@@ -1,10 +1,13 @@
+import DifficultiesController from "#controllers/api/v1/difficulties_controller";
 import OrganizationsController from "#controllers/api/v1/organizations_controller";
 import { middleware } from "#start/kernel";
 import router from "@adonisjs/core/services/router";
 
 router.group(() => {
-  router.get('/organization', [OrganizationsController])
-}).prefix('/api/v1').use([
+  router.get('/organization', [OrganizationsController]).as('organization')
+
+  router.resource('difficulties', DifficultiesController).apiOnly()
+}).prefix('/api/v1').as('api').use([
   middleware.forceJsonResponse(),
   middleware.auth({ guards: ['api'] }),
   middleware.organization()

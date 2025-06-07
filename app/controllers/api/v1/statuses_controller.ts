02de4111c700ed4050d2c0473cf95cf59b53662a
Tom Gobich <tom@tomgobich.com>
2025-06-07 09:05:41 -0400

    lesson(4.6): statuses api crud

---
+++ app/controllers/api/v1/statuses_controller.ts
@@ -0,0 +1,63 @@
+import DestroyStatus from '#actions/statuses/destroy_status'
+import StoreStatus from '#actions/statuses/store_status'
+import UpdateStatus from '#actions/statuses/update_status'
+import { withOrganizationMetaData } from '#validators/helpers/organizations'
+import { statusDestroyValidator, statusValidator } from '#validators/status'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class StatusesController {
+  /**
+     * Display a list of resource
+     */
+    async index({ organization }: HttpContext) {
+      return organization.related('statuses').query().orderBy('order')
+    }
+  
+    /**
+     * Handle form submission for the create action
+     */
+    async store({ request, organization }: HttpContext) {
+      const data = await request.validateUsing(statusValidator)
+      const status = await StoreStatus.handle({ organization, data })
+  
+      return status
+    }
+  
+    /**
+     * Show individual record
+     */
+    async show({ params, organization }: HttpContext) {
+      return organization.related('statuses').query()
+        .where({ id: params.id })
+        .firstOrFail()
+    }
+  
+    /**
+     * Handle form submission for the edit action
+     */
+    async update({ params, request, organization }: HttpContext) {
+      const data = await request.validateUsing(statusValidator)
+      const status = await UpdateStatus.handle({ 
+        id: params.id, 
+        organization, 
+        data 
+      })
+  
+      return status
+    }
+  
+    /**
+     * Delete record
+     */
+    async destroy({ request, response, params, organization }: HttpContext) {
+      const data = await request.validateUsing(statusDestroyValidator, withOrganizationMetaData(organization.id))
+  
+      await DestroyStatus.handle({ 
+        id: params.id,
+        organization,
+        data
+      })
+  
+      return response.status(204)
+    }
+}
--- start/routes/api/_v1.ts
+++ start/routes/api/_v1.ts
@@ -1,6 +1,7 @@
 import AccessLevelsController from "#controllers/api/v1/access_levels_controller";
 import DifficultiesController from "#controllers/api/v1/difficulties_controller";
 import OrganizationsController from "#controllers/api/v1/organizations_controller";
+import StatusesController from "#controllers/api/v1/statuses_controller";
 import { middleware } from "#start/kernel";
 import router from "@adonisjs/core/services/router";
 
@@ -9,6 +10,7 @@ router.group(() => {
 
   router.resource('difficulties', DifficultiesController).apiOnly()
   router.resource('access-levels', AccessLevelsController).apiOnly()
+  router.resource('statuses', StatusesController).apiOnly()
 }).prefix('/api/v1').as('api').use([
   middleware.forceJsonResponse(),
   middleware.auth({ guards: ['api'] }),

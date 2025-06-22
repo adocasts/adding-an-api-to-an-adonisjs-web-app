328a4a865f9447d223ea0bffed8c756e4d85aa2a
Tom Gobich <tom@tomgobich.com>
2025-06-22 12:21:03 -0400

    lesson(5.1): creating, updating, and deleting courses

--- app/controllers/api/v1/courses_controller.ts
+++ app/controllers/api/v1/courses_controller.ts
@@ -1,3 +1,9 @@
+import AuthorizeToken from '#actions/abilities/authorize_token'
+import DestroyCourse from '#actions/courses/destroy_course'
+import StoreCourse from '#actions/courses/store_course'
+import UpdateCourse from '#actions/courses/update_course'
+import { courseValidator } from '#validators/course'
+import { withOrganizationMetaData } from '#validators/helpers/organizations'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class CoursesController {
@@ -5,6 +11,8 @@ export default class CoursesController {
    * Display a list of resource
    */
   async index({ organization }: HttpContext) {
+    AuthorizeToken.read(organization)
+
     const courses = await organization
       .related('courses')
       .query()
@@ -21,7 +29,14 @@ export default class CoursesController {
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, organization }: HttpContext) {
+    AuthorizeToken.create(organization)
+
+    const data = await request.validateUsing(courseValidator, withOrganizationMetaData(organization.id))
+    const course = await StoreCourse.handle({ organization, data })
+
+    return course
+  }
 
   /**
    * Show individual record
@@ -31,10 +46,27 @@ export default class CoursesController {
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, organization }: HttpContext) {
+    AuthorizeToken.update(organization)
+
+    const data = await request.validateUsing(courseValidator, withOrganizationMetaData(organization.id))
+    const course = await UpdateCourse.handle({
+      id: params.id,
+      organization,
+      data
+    })
+
+    return course
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ response, params, organization }: HttpContext) {
+    AuthorizeToken.delete(organization)
+
+    await DestroyCourse.handle({ organization, id: params.id })
+
+    return response.status(204)
+  }
 }

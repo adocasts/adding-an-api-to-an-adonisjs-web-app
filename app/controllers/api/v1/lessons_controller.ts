ea7c1c627099ff7502cdb1afcfe330fbd3e1c74c
Tom Gobich <tom@tomgobich.com>
2025-07-22 19:19:10 -0400

    lesson(5.8): creating, updating, patching, and deleting lessons

---
+++ app/controllers/api/v1/lessons_controller.ts
@@ -0,0 +1,70 @@
+import AuthorizeToken from '#actions/abilities/authorize_token'
+import DestroyLesson from '#actions/lessons/destroy_lesson'
+import StoreLesson from '#actions/lessons/store_lesson'
+import UpdateLesson from '#actions/lessons/update_lesson'
+import UpdateLessonTag from '#actions/lessons/update_lesson_tag'
+import { withOrganizationMetaData } from '#validators/helpers/organizations'
+import { lessonPatchTagValidator, lessonValidator } from '#validators/lesson'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class LessonsController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, organization }: HttpContext) {
+    AuthorizeToken.create(organization)
+
+    const data = await request.validateUsing(lessonValidator, withOrganizationMetaData(organization.id))
+
+    return StoreLesson.handle({
+      organization,
+      data
+    })
+  }
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request, organization }: HttpContext) {
+    AuthorizeToken.update(organization)
+
+    const data = await request.validateUsing(lessonValidator, withOrganizationMetaData(organization.id))
+
+    return UpdateLesson.handle({
+      id: params.id,
+      organization,
+      data
+    })
+  }
+
+  async tag({ params, request, organization }: HttpContext) {
+    AuthorizeToken.update(organization)
+
+    const data = await request.validateUsing(lessonPatchTagValidator, withOrganizationMetaData(organization.id))
+
+    return UpdateLessonTag.handle({
+      id: params.id,
+      organization,
+      data
+    })
+  }
+
+  /**
+   * Delete record
+   */
+  async destroy({ params, response, organization }: HttpContext) {
+    AuthorizeToken.delete(organization)
+
+    await DestroyLesson.handle({
+      id: params.id,
+      organization
+    })
+
+    return response.status(204)
+  }
+}
--- start/routes/api/_v1.ts
+++ start/routes/api/_v1.ts
@@ -1,25 +1,34 @@
-import AccessLevelsController from "#controllers/api/v1/access_levels_controller";
-import CoursesController from "#controllers/api/v1/courses_controller";
-import DifficultiesController from "#controllers/api/v1/difficulties_controller";
-import ModulesController from "#controllers/api/v1/modules_controller";
-import OrganizationsController from "#controllers/api/v1/organizations_controller";
-import StatusesController from "#controllers/api/v1/statuses_controller";
-import { middleware } from "#start/kernel";
-import router from "@adonisjs/core/services/router";
+const AccessLevelsController = () => import('#controllers/api/v1/access_levels_controller')
+const CoursesController = () => import('#controllers/api/v1/courses_controller')
+const DifficultiesController = () => import('#controllers/api/v1/difficulties_controller')
+const LessonsController = () => import('#controllers/api/v1/lessons_controller')
+const ModulesController = () => import('#controllers/api/v1/modules_controller')
+const OrganizationsController = () => import('#controllers/api/v1/organizations_controller')
+const StatusesController = () => import('#controllers/api/v1/statuses_controller')
+import { middleware } from '#start/kernel'
+import router from '@adonisjs/core/services/router'
 
-router.group(() => {
-  router.get('/organization', [OrganizationsController]).as('organization')
+router
+  .group(() => {
+    router.get('/organization', [OrganizationsController]).as('organization')
 
-  router.resource('difficulties', DifficultiesController).apiOnly()
-  router.resource('access-levels', AccessLevelsController).apiOnly()
-  router.resource('statuses', StatusesController).apiOnly()
-  router.resource('courses', CoursesController).apiOnly()
-  router.resource('courses.modules', ModulesController).apiOnly().except(['show'])
+    router.resource('difficulties', DifficultiesController).apiOnly()
+    router.resource('access-levels', AccessLevelsController).apiOnly()
+    router.resource('statuses', StatusesController).apiOnly()
+    router.resource('courses', CoursesController).apiOnly()
+    router.resource('courses.modules', ModulesController).apiOnly().except(['show'])
+    router.resource('lessons', LessonsController).apiOnly().except(['show'])
 
-  router.patch('/courses/:id/tag', [CoursesController, 'tag']).as('courses.tag')
-  router.patch('/courses/:course_id/modules/:id/tag', [ModulesController, 'tag']).as('courses.modules.tag')
-}).prefix('/api/v1').as('api.v1').use([
-  middleware.forceJsonResponse(),
-  middleware.auth({ guards: ['api'] }),
-  middleware.organization()
-])
+    router.patch('/courses/:id/tag', [CoursesController, 'tag']).as('courses.tag')
+    router
+      .patch('/courses/:course_id/modules/:id/tag', [ModulesController, 'tag'])
+      .as('courses.modules.tag')
+    router.patch('/lessons/:id/tag', [LessonsController, 'tag']).as('lessons.tag')
+  })
+  .prefix('/api/v1')
+  .as('api.v1')
+  .use([
+    middleware.forceJsonResponse(),
+    middleware.auth({ guards: ['api'] }),
+    middleware.organization(),
+  ])
